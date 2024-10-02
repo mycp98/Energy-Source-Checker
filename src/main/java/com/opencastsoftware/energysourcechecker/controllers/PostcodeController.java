9adb76d087477ecc172e5b96d6df4b89a77c0ea6
Chris Cook <114592674+ChrisCookOC@users.noreply.github.com>
2024-10-02 15:14:44 +0100

    Database name the same each time Request body needed

--- src/main/java/com/opencastsoftware/energysourcechecker/controllers/PostcodeController.java
+++ src/main/java/com/opencastsoftware/energysourcechecker/controllers/PostcodeController.java
@@ -1,11 +1,14 @@
 package com.opencastsoftware.energysourcechecker.controllers;
 
 import com.opencastsoftware.energysourcechecker.services.PostcodeService;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log
 @RestController
 public class PostcodeController {
 
@@ -13,7 +16,10 @@ public class PostcodeController {
     private PostcodeService postcodeService;
 
     @PostMapping("/postcode")
-    public void postcode(String postcode) {
+    public void postcode(@RequestBody String postcode) {
+
+        log.info("Postcode entered is " + postcode);
+
         postcodeService.createPostcode(postcode);
     }
 }
